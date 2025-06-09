ee3103286b8c41536bd0188abed5f5c653705e75
Sai Chaitanya Pachipulusu <chaitanya17.sai@gmail.com>
2025-06-08 22:06:46 -0400

    Updated

--- pages/api/get-conversation.js
+++ pages/api/get-conversation.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../lib/mongodb';
-import Conversation from '../../lib/Conversation';
+//import dbConnect from '../../lib/mongodb';
+//import Conversation from '../../lib/Conversation';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
